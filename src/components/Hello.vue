741b4025a74d9bcb10924951d7b07cf84638f392
xiangcatherine <xiang.catherine@gmail.com>
2017-06-09 09:27:29 -0400

    First commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1 class="title">hueist</h1>
+    <h2>how are you?</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
+}
+
+h1 {
+  letter-spacing: .5rem;
+}
+
+.title {
+  left: 0;
+}
+</style>
