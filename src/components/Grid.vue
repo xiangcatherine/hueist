efa0cf199bb4d9528ee86ad830f06adf7be7a480
xiangcatherine <xiang.catherine@gmail.com>
2017-06-11 22:08:15 -0400

    Add header, color grid, and toggle componenets

---
+++ src/components/Grid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="grid">
+    <template v-for="row in colorGrid">
+      <div class="grid-row">
+        <template v-for="color in row">
+    	    <div class="grid-cell" :style="{background: color}"></div>
+   	    </template>
+ 	    </div>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        colorGrid: [
+          ['#ddd', '#ccc', '#ddd'],
+          ['#ccc', '#ddd', '#ccc'],
+          ['#ddd', '#ccc', '#ddd']
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .grid {
+  	width: 55vmin;
+  	height: 55vmin;
+  	margin: auto;
+  }
+
+  .grid-row {
+  	width: 100%;
+  	height: 33%;
+  }
+
+  .grid-cell {
+  	display: inline-block;
+  	width: 33%;
+  	height: 100%;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <div>    
+      <a href="" class="logo">hueist</a>
+    </div>
+
+    <div class="right">
+      <template v-if="authenticated">
+        <a href="">settings</a>
+      </template> 
+      <template v-else>
+        <a href="">log in</a>
+        <a href="">sign up</a>
+      </template>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        authenticated: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  nav {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+    padding: 2rem;
+  }
+
+  .right {
+    margin-left: auto;
+  }
+
+  a {
+    color: #222;
+    text-decoration: none;
+    padding: 2rem 1.5rem;
+  }
+
+  a:hover,
+  a:focus,
+  a:active {
+    background: #fff8ea;
+  }
+</style>
