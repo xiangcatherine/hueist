b8e34569c35b4284f36fe5b7dec545bbb2facc81
xiangcatherine <xiang.catherine@gmail.com>
2017-06-11 22:09:23 -0400

    Add home and choose routes and toplevel components

---
+++ src/components/Choose.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>how are you?</h1>
+    <app-grid></app-grid>
+    <app-toggle></app-toggle>
+  </div>
+</template>
+
+<script>
+  import Grid from './Grid'
+  import Toggle from './Toggle'
+
+  export default {
+    name: 'choose',
+    components: {
+      appGrid: Grid,
+      appToggle: Toggle
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>use color to track your mood</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  h1 {
+    letter-spacing: .5rem;
+  }
+
+  h2 {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  .title {
+    margin-left: 20px;
+    margin-top: 40px;
+  }
+</style>
