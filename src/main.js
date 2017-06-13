bb22a26133c3cced1b520cabfe0e5b966e7c6c9d
xiangcatherine <xiang.catherine@gmail.com>
2017-06-13 18:58:11 -0400

    Add vuex module

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App'
 import router from './router'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.use(Vuex)
 Vue.use(VueResource)
+Vue.use(VueRouter)
+
+// export default new Vuex.Store({
+// })
 
 /* eslint-disable no-new */
 new Vue({
