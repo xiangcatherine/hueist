e715fd9346dbc0316b9a93b98fe9fac412e17429
xiangcatherine <xiang.catherine@gmail.com>
2017-06-16 00:04:10 -0400

    Finish homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>use color to track your mood</h1>
+    <h1>track your mood<br><br>in full color</h1>
+    <div class="jumping-squares">
+      <div class="jumping-square one"></div>
+      <div class="jumping-square two"></div>
+      <div class="jumping-square three"></div>
+    </div>
   </div>
 </template>
 
@@ -22,13 +27,47 @@ export default {
 
 <style scoped>
   h1 {
-    font-weight: normal;
+    margin-top: 3rem;
     text-align: center;
-    margin-top: 20px;
+    font-size: 2rem;
+    font-weight: normal;
   }
 
-  .title {
-    margin-left: 20px;
-    margin-top: 40px;
+  .jumping-squares {
+    width: 40rem;
+    margin: auto;
+    margin-top: 5rem;
+    text-align: center;
+  }
+
+  .jumping-square {
+    display: inline-block;
+    width: 10rem;
+    height: 10rem;
+    margin: 1rem;
+    animation: jumping 1.5s ease 2;
+  }
+
+  .one {
+    background: #fadaa1;
+    animation-delay: .4s;
+  }
+
+  .two {
+    background: #b0d4a2;
+    animation-delay: .2s; 
+  }
+
+  .three {
+    background: #a0a8c9;
+    animation-delay: .6s;
+  }
+
+  @keyframes jumping {
+    0% { transform: translateY(0rem); }
+    70% { transform: translateY(0rem); }
+    80% { transform: translateY(-4rem); }  
+    90% { transform: translateY(-4rem); }   
+    100% { transform: translateY(0rem); }
   }
 </style>
